c0ba3870a47ca8796619367a9449f5097484a1be
Catalin <leca.catalin1995@gmail.com>
2021-01-08 23:50:10 +0200

    lecture 189 - finished signup tests

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,79 @@
+import request from 'supertest';
+import { app } from "../../app";
+
+it('return a 201 on successful signup', async () => {
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201) // status code of 201
+})
+
+it('returns a 400 with an invalid email', async () => {
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'asdasdasd',
+            password: 'password'
+        })
+        .expect(400) // status code of 201
+})
+
+it('returns a 400 with an invalid password', async () => {
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'asdasdasd',
+            password: 'a'
+        })
+        .expect(400) // status code of 201
+})
+
+it('returns a 400 with missing email and password', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'asdasdasd@mail.com',
+        })
+        .expect(400) // status code of 201
+
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            password: 'asdasdasdasda'
+        })
+        .expect(400) // status code of 201
+})
+
+it('disallows duplicate emails', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201)
+
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(400)
+})
+
+it('sets a cookie after successful signup', async () => {
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201)
+
+    expect(response.get('Set-Cookie')).toBeDefined()
+
+})
---
+++ auth/src/test/setup.ts
@@ -0,0 +1,32 @@
+import {MongoMemoryServer} from "mongodb-memory-server"
+import mongoose from 'mongoose'
+import {app} from "../app"
+
+let mongo: any;
+beforeAll(async () => {
+    process.env.JWT_KEY = 'asdfasdf';
+
+    mongo = new MongoMemoryServer();
+    const mongoUri = await mongo.getUri();
+
+    await mongoose.connect(mongoUri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+});
+
+beforeEach(async () => {
+    // before each test we are resetting all the data in the db
+
+    const collections = await mongoose.connection.db.collections();
+
+    for (let collection of collections) {
+        await collection.deleteMany({})
+    }
+});
+
+afterAll(async () => {
+    await mongo.stop();
+    await mongoose.connection.close();
+
+})
