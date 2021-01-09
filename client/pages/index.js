9cfd6a7ed6837eab862b298779cb4004ee5a4fe2
Catalin <leca.catalin1995@gmail.com>
2021-01-09 23:56:53 +0200

    lecture 221 - configured getInitialProps and cookies, big theoretical time

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,25 @@
-export default () => {
-  return <h1>Landing Page</h1>
+import axios from 'axios';
+
+const LandingPage = ({currentUser}) => {
+  console.log(currentUser);
+  return (
+    <h1>Landing Page</h1>
+  )
 }
+
+LandingPage.getInitialProps = async ({ req }) => {
+
+  if (typeof window === 'undefined') {
+    const {data} = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
+        headers: req.headers
+      }
+    )
+    return data
+  } else {
+    const { data } = await axios.get('/api/users/currentuser')
+    return data;
+  }
+}
+
+export default LandingPage;
