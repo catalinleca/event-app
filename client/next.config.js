35af9cc8b3142d57c1bde9a1afa156fbb13220a7
Catalin <leca.catalin1995@gmail.com>
2021-01-09 13:57:49 +0200

    lecture 202 - next config file

---
+++ client/next.config.js
@@ -0,0 +1,10 @@
+// next will be executed with some webpack config
+// in stead of watching for file changes in an automated fashion
+// pull all changes once every 300 ms
+
+module.exports = {
+  webpackDevMiddleware: config => {
+    config.watchOptions.poll = 300;
+    return config;
+  }
+}
