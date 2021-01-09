f5610a0e6efb837501c93fff93d99dbba107b2c3
Catalin <leca.catalin1995@gmail.com>
2021-01-10 00:05:05 +0200

    lecture 222 - restructured getInitialProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({currentUser}) => {
   console.log(currentUser);
@@ -7,19 +7,9 @@ const LandingPage = ({currentUser}) => {
   )
 }
 
-LandingPage.getInitialProps = async ({ req }) => {
-
-  if (typeof window === 'undefined') {
-    const {data} = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-        headers: req.headers
-      }
-    )
-    return data
-  } else {
-    const { data } = await axios.get('/api/users/currentuser')
-    return data;
-  }
+LandingPage.getInitialProps = async (context) => {
+  const { data } = await buildClient(context).get('/api/users/currentuser');
+  return data;
 }
 
 export default LandingPage;
