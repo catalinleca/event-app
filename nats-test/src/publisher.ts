1ebd31c568cee3032d4fc5aca6adb8cd256cb289
Catalin Leca <mihaicatalin.leca@vodafone.com>
2021-03-19 00:56:25 +0200

    lecture 306 - before moving to common project

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -7,12 +7,18 @@ const stan = nats.connect('ticketing', 'abc', {
     url: 'http://localhost:4222'
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
     console.log('Publisher connected to NATS')
 
-    new TicketCreatedPublisher(stan).publish({
-        id: '123',
-        title: 'concert',
-        price: 20
-    });
+    const publisher = new TicketCreatedPublisher(stan)
+    try {
+        await publisher.publish({
+            id: '123',
+            title: 'concert',
+            price: 20
+        });
+    } catch (err) {
+        console.error(err)
+    }
+
 });
