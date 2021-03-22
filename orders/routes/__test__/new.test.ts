d4fe75cfa5cf5dd5173039ef7a200f95f6a8faa8
Catalin Leca <mihaicatalin.leca@vodafone.com>
2021-03-23 00:48:22 +0200

    lecture 344 - added tests

---
+++ orders/routes/__test__/new.test.ts
@@ -0,0 +1,52 @@
+import {app} from "../../src/app";
+import request from 'supertest';
+import mongoose from "mongoose";
+import {Ticket} from "../../models/ticket";
+import {Order} from "../../models/orders";
+import {OrderStatus} from "@cltickets/common";
+
+it('returns an error if the ticket does not exist', async () => {
+  const ticketId = mongoose.Types.ObjectId()
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ticketId})
+    .expect(404)
+})
+
+it('returns an error if the ticket is already reserved', async () => {
+  const ticket = Ticket.build({
+    title: 'Concert',
+    price: 20
+  })
+  await ticket.save();
+
+  const order = Order.build({
+    ticket,
+    userId: 'asdasda',
+    status: OrderStatus.Created,
+    expiresAt: new Date()
+  })
+  await order.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ticketId: ticket.id})
+    .expect(400)
+})
+
+it('reserves a ticket', async () => {
+  const ticket = Ticket.build({
+    title: 'Concert',
+    price: 20
+  })
+  await ticket.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ticketId: ticket.id})
+    .expect(201)
+})
