f06eb823861a0362f7e28119630d3ae7f32cf08a
Catalin Leca <mihaicatalin.leca@vodafone.com>
2021-03-22 21:05:45 +0200

    lecture 329 - added orders service error free

---
+++ orders/src/app.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import 'express-async-errors';
+import {json} from 'body-parser';
+import cookieSession from "cookie-session";
+
+import {errorHandler, NotFoundError, currentUser} from "@cltickets/common";
+
+import {deleteOrderRouter} from "../routes/delete";
+import {newOrderRouter} from "../routes/new";
+import {showOrderRouter} from "../routes/show";
+import {indexOrderRouter} from "../routes";
+
+
+const app = express();
+app.set('trust proxy', true);
+
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false, // disable encryption
+    // secure: true // enable cookie usage only for https connection
+    secure: process.env.NODE_ENV !== 'test'
+  })
+);
+app.use(currentUser);
+
+app.use(deleteOrderRouter);
+app.use(indexOrderRouter)
+app.use(newOrderRouter)
+app.use(showOrderRouter)
+
+app.all('*', async () => {
+  throw new NotFoundError();
+})
+
+app.use(errorHandler);
+
+export { app };
