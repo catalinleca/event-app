37e2b7889063a062f3ffc0792604ffabc54e4d0c
Catalin Leca <mihaicatalin.leca@vodafone.com>
2021-03-23 10:52:45 +0200

    lecture 352 - added order created and cancelled events in common lib

--- tickets/src/events/publishers/ticket-updated-publisher.ts
+++ tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -1,6 +1,6 @@
 import { Publisher, Subjects, TicketUpdatedEvent } from "@cltickets/common";
 
 export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
-  subject: Subjects.TicketsUpdated = Subjects.TicketsUpdated;
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
 }
 
