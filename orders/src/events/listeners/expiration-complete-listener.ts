35e512e9a68f0e728773f80ece370852883d6478
Catalin Leca <mihaicatalin.leca@vodafone.com>
2021-03-25 20:38:46 +0200

    Section 20 / lecture 420 - don't cancel completed orders

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -14,6 +14,9 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
     if (!order) {
       throw new Error("Order Not Found");
     }
+    if (order.status === OrderStatus.Complete) {
+      return msg.ack();
+    }
 
     order.set({
       status: OrderStatus.Cancelled
