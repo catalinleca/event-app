2169e8a19f7ca80721ff10de512e7485fa044b30
Catalin Leca <mihaicatalin.leca@vodafone.com>
2021-03-26 23:56:27 +0200

    lecture 451 - two quick fixes

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,26 +1,30 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import buildClient from '../api/build-client';
-import Header from "../components/header"
+import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
+import Header from "../components/header";
 
-const AppComponent = ({Component, pageProps, currentUser}) => {
+const AppComponent = ({
+  Component,
+  pageProps,
+  currentUser
+}) => {
   console.log(pageProps);
   return (
     <div>
       <Header currentUser={currentUser}/>
-      <Component {...pageProps} />
+      <Component currentUser={currentUser} {...pageProps} />
     </div>
-  )
-}
+  );
+};
 
 AppComponent.getInitialProps = async (appContext) => {
   const client = buildClient(appContext.ctx);
-  const {data} = await client.get('/api/users/currentuser');
+  const { data } = await client.get("/api/users/currentuser");
 
   // Invoking LandingPage getInitialProps
   // Component - the component that it wraps
   let pageProps;
   if (appContext.Component.getInitialProps) {
-    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx, client, data.currentUser);
   }
 
   console.log(pageProps);
@@ -29,6 +33,6 @@ AppComponent.getInitialProps = async (appContext) => {
     pageProps,
     ...data
   };
-}
+};
 
 export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,9 @@
-import buildClient from '../api/build-client';
-
 const LandingPage = ({currentUser}) => {
   return currentUser ? <h1>You are signed in </h1> : <h1>You are not sign in</h1>
 }
 
-LandingPage.getInitialProps = async (context) => {
-  const { data } = await buildClient(context).get('/api/users/currentuser');
-  return data;
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  return {};
 }
 
 export default LandingPage;
-
