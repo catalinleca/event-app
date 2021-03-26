8bc5cd30d50deccdcc14f3fb9977d11cc2860244
Catalin Leca <mihaicatalin.leca@vodafone.com>
2021-03-27 01:45:50 +0200

    Section 22 / lecture 467 - client done

---
+++ client/pages/orders/index.js
@@ -0,0 +1,20 @@
+const OrderIndex = ({ orders }) => (
+  <ul>
+    {orders.map((order) => (
+      <li key={order.id}>
+        {order.ticket.title}
+        {' '}
+        -
+        {order.status}
+      </li>
+    ))}
+  </ul>
+);
+
+OrderIndex.getInitialProps = async (context, client) => {
+  const { data } = await client.get('/api/orders');
+
+  return { orders: data };
+};
+
+export default OrderIndex;
