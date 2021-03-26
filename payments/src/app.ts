e49f17b70c13a8042e905f113bcb253eac7b83c4
Catalin Leca <mihaicatalin.leca@vodafone.com>
2021-03-26 12:01:04 +0200

    lecture 432 - adding create charge order route and setting route in ingress

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -4,6 +4,7 @@ import {json} from 'body-parser';
 import cookieSession from "cookie-session";
 
 import {errorHandler, NotFoundError, currentUser} from "@cltickets/common";
+import { createChargeRouter } from "./routes/new";
 
 const app = express();
 app.set('trust proxy', true);
@@ -17,6 +18,8 @@ app.use(
   })
 );
 app.use(currentUser);
+
+app.use(createChargeRouter)
 
 app.all('*', async () => {
   throw new NotFoundError();
